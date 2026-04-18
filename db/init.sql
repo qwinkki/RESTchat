0a70eb8e14548efc345c1a2cd572acf739f5d6d9
qwinkki <rost77.ovchinnikov@gmail.com>
2026-04-19 00:44:15 +0300

    postgres table

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS messages (
     id          SERIAL PRIMARY KEY,
-    user        TEXT NOT NULL,
+    "user"        TEXT NOT NULL,
     text        TEXT NOT NULL,
     createdAt   TIMESTAMP DEFAULT NOW()
 );
